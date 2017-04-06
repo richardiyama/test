8810ad1312f921504e268b7d38ec4550cf8149b7
richard iyama <richard.iyama@orbsolutions.com.ng>
2017-04-06 16:50:59 +0100

    Added search and Toast alert

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -11,10 +11,13 @@
     </div>
   
     <div class="layout-view">
-    <q-search class="orange"  v-model="searchModel"></q-search>
+      <q-search class="orange" 
+                v-model="search"></q-search>
       <div class="list">
-        <div v-for="i in filteredTasks()" :key="i.id" class="item">
-
+        <div v-for="i in filteredTasks"
+             :key="i.id"
+             class="item">
+  
           <i class="item-primary">lightbulb_outline</i>
           <div class="item-content has-secondary">
             <div>{{i.title}}</div>
@@ -22,18 +25,18 @@
   
           <div class="item-secondary">
             <i slot="target">
-                more_vert
-                <q-popover :ref="'popover'">
-                  <div class="list">
-                    <div class="item item-link" @click="edit(i.id)">
-                      <div class="item-content">Edit</div>
-                    </div>
-                    <div class="item item-link" @click="deleteItem(i.id)">
-                      <div class="item-content">Delete</div>
-                    </div>
-                  </div>
-                </q-popover>
-              </i>
+                      more_vert
+                      <q-popover :ref="'popover'">
+                        <div class="list">
+                          <div class="item item-link" @click="edit(i.id)">
+                            <div class="item-content">Edit</div>
+                          </div>
+                          <div class="item item-link" @click="deleteItem(i.id)">
+                            <div class="item-content">Delete</div>
+                          </div>
+                        </div>
+                      </q-popover>
+                    </i>
           </div>
         </div>
       </div>
@@ -43,10 +46,10 @@
 
 <script>
 import _ from 'lodash'
-import Quasar, { Utils, Dialog,LocalStorage } from 'quasar'
+import Quasar, { Utils, Dialog, LocalStorage, Toast } from 'quasar'
 
-var lastCount = 0 
-function counter () {
+var lastCount = 0
+function counter() {
   lastCount += 1
   return lastCount
 }
@@ -54,24 +57,31 @@ function counter () {
 export default {
   data() {
     return {
-      searchs: '',
-      taskList: LocalStorage.get.item('tasks',[]),
-      searchModel: ""
-    
+      search: '',
+      taskList: LocalStorage.get.item('tasks', []),
+      
+
     }
   },
 
-
-  methods: {
-    filteredTasks() {
-      if (this.searchs.length > 0) {
-        retv = _.filter(this.taskList, { label: this.searchModel })
+  computed: {
+    filteredTasks: function () {
+    
+      if (this.search.length > 0) {
+        var retv = _.filter(this.taskList, { title: this.search})
+    
+        //console.log(matches_array)
         return retv
+      
       }
 
       return this.taskList
-    },
+    }
 
+  },
+
+  methods: {
+     
     add() {
       var self = this
       Dialog.create({
@@ -88,20 +98,23 @@ export default {
           'Delete',
           {
             label: 'Create',
-            handler(data) {
+            preventClose: true,
 
-              if(data.task == 0){
-                alert("Warning!!!...You are trying to submit an empty field")
-              }
+            handler(data, close) {
 
-              else if(data.task != 0){
-              var task = {id:counter(), title: data.task, status: 0}
-              self.taskList.push(task)
-              LocalStorage.set('tasks',self.taskList)
-              alert("You are creating a new task..Tick to continue")
+              if (data.task == 0) {
+                Toast.create('Warning!!!...You are trying to submit an empty field')
+                return
               }
-              
-              
+              close(() => {
+                var task = { id: counter(), title: data.task, status: 0 }
+                self.taskList.push(task)
+                LocalStorage.set('tasks', self.taskList)
+
+              })
+
+
+
             }
           }
         ]
@@ -113,7 +126,7 @@ export default {
       var self = this
       // var task = _.find(this.taskList, {id:id})
       var task = {}
-      this.taskList.forEach(function(el) {
+      this.taskList.forEach(function (el) {
         if (el.id === id) {
           task = el
         }
@@ -133,20 +146,19 @@ export default {
           'Delete',
           {
             label: 'Update',
-            handler(data) {
-                if(data.task == 0){
-                alert("Warning!!!...You are trying to submit an empty field")
+            preventClose: true,
+            handler(data, close) {
+              if (data.task == 0) {
+                Toast.create('Warning!!!...You are trying to submit an empty field')
+                return
               }
-               
-               else if(data.task != 0){
-              task.title = data.task
-               LocalStorage.set('tasks', self.taskList)
-              //self.taskList.push(task.title)
-              //LocalStorage.set('tasks',task.title)
-               alert("You are updating..Tick to continue")
-              }
-              
-              
+
+              close(() => {
+                task.title = data.task
+                LocalStorage.set('tasks', self.taskList)
+              })
+
+
             }
           }
         ]
@@ -157,25 +169,25 @@ export default {
     deleteItem(id) {
       var self = this
       var task = {}
-      this.taskList.forEach(function(el) {
-        if(el.id == id){
+      this.taskList.forEach(function (el) {
+        if (el.id == id) {
           task = el
           console.log(task)
-          self.taskList.splice(self.taskList.indexOf(task),1)
+          self.taskList.splice(self.taskList.indexOf(task), 1)
           LocalStorage.set('tasks', self.taskList)
-      
-          }
-          
+
+        }
+
       })
-    
-    } 
-    },
+
+    }
+  },
   mounted() {
 
     // create task store if not exist
-      if (LocalStorage.has('tasks') === false) {
-        LocalStorage.set('tasks', [])
+    if (LocalStorage.has('tasks') === false) {
+      LocalStorage.set('tasks', [])
+    }
   }
-}
 }
 </script>
