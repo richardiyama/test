199c6c9c902ab3e8252eb6686e43e296571d5d71
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-07-29 05:40:21 +0100

    Update Index.vue

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -36,6 +36,7 @@
                            @click.native="deleteItem(i.id)"
                            icon="delete">
               </q-small-fab>
+            
             </q-fab>
           </div>
         </div>
